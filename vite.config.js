f9746ff97fa307b02785d447049b23ee3a2d188a
David Bijl <davidbijl321@gmail.com>
2025-04-23 17:11:31 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import path from 'node:path';
 const __dirname = import.meta.dirname;
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     publicDir: 'client',
@@ -10,7 +9,10 @@ export default defineConfig({
     emptyOutDir: false,
     outDir: 'dist',
     rollupOptions: {
-      input: path.resolve(__dirname, 'client/index.js'),
+      input: {
+        main: path.resolve(__dirname, 'client/index.js'),
+        style: path.resolve(__dirname, 'client/index.css')
+      },
       output: {
         entryFileNames: '[name].js',
         chunkFileNames: '[name].js',
